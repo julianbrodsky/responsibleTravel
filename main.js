357c4a29f3086f85e59e303295620dbf821a2050
Julian Brodsky <brodskyjulian@yahoo.com>
2022-07-07 13:50:35 -0600

    linked in covid api

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
+document.querySelector('#submitCaseInput').addEventListener('click', searchCountryCases)
+
+function searchCountryCases(){
+
+let userCountry = document.querySelector('#caseInput').value
+
 fetch(`https://api.covid19api.com/countries`)
     .then(res => res.json())
     .then(data => {
         console.log(data)
-
+        let ind = data.findIndex( obj => {
+            return(obj.Country === userCountry)
+        })
+    document.querySelector('#countryName').innerText = `Showing cases for ${userCountry}`
+    Window.slug = data[ind].slug
     })
+
+fetch(`https://api.covid19api.com/dayone/country/france`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+}
