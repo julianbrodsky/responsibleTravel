b9e106474e3f4fc5e834e2c6cf7c20ef325456e5
Julian Brodsky <brodskyjulian@yahoo.com>
2022-07-07 16:16:33 -0600

    Lookup total cases by country

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ function searchCountryCases(){
 
 let userCountry = document.querySelector('#caseInput').value
 
+let slug = ''
+
 fetch(`https://api.covid19api.com/countries`)
     .then(res => res.json())
     .then(data => {
@@ -12,12 +14,27 @@ fetch(`https://api.covid19api.com/countries`)
             return(obj.Country === userCountry)
         })
     document.querySelector('#countryName').innerText = `Showing cases for ${userCountry}`
-    Window.slug = data[ind].slug
+    slug = data[ind].Slug
+    console.log(slug)
+
     })
 
-fetch(`https://api.covid19api.com/dayone/country/france`)
+    fetch(`https://api.covid19api.com/summary`)
     .then(res => res.json())
     .then(data => {
-        console.log(data)
+
+        let index = data.Countries.findIndex( obj => {
+            return(obj.Country === userCountry)})
+
+        let countryData = data.Countries[index]
+        document.querySelector('#caseCount').innerText = `Total confirmed cases in ${userCountry}: ${countryData.TotalConfirmed}`
     })
+
+// fetch(`https://api.covid19api.com/live/country/${slug}/status/confirmed`)
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data)
+//         console.log(slug)
+//         console.log(`https://api.covid19api.com/live/country/${slug}/status/confirmed`)
+//     })
 }
